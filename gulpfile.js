7d72cedcbb8b3bdc329da085a580e9e1a226ac06
poonamjd <poonam.smiles@gmail.com>
2017-12-06 14:16:42 +0530

    Initial release

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+//Styles
+gulp.task('styles',function(){
+    console.log('starting styles task');
+})
+
+
+//Scripts
+gulp.task('scripts',function(){
+    console.log('starting scripts task');
+
+    return gulp.src('public/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist'));
+})
+
+
+//Images
+gulp.task('images',function(){
+    console.log('starting images task');
+})
+
+//videos
+gulp.task('default',function(){
+    console.log('starting videos task');
+})
---
+++ public/dist/demo2.js
@@ -0,0 +1,1 @@
+console.log("Init \n\n"),console.log("Pull \n\n"),console.log("Commit \n\n"),console.log("Push \n\n");
